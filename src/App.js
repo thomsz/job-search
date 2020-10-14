9734c345ed06a0f2f597028bc45a771c8524b29e
Tom Segev <tom@golaender.com>
2020-10-14 14:48:09 +0300

    Add basic layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from "react";
+import { Layout } from "antd";
+import * as classes from "./App.module.css";
 import MainPanel from "./components/MainPanel/MainPanel";
+
+const { Content, Footer } = Layout;
 
 function App() {
   return (
-    <div className="App">
-      <h2>Welcome!</h2>
-      <MainPanel />
-    </div>
+    <Layout className="layout">
+      <Content style={{ padding: "0 50px" }}>
+        <div className={classes["site-layout-content"]}>
+          <MainPanel />
+        </div>
+      </Content>
+      <Footer />
+    </Layout>
   );
 }
 
