08815fcef080f2ec3875e4baf834836817ff60a7
Tom Segev <tom@golaender.com>
2020-10-16 10:41:01 +0300

    Post formatting

--- src/components/MainPanel/Search/Results/Results.js
+++ src/components/MainPanel/Search/Results/Results.js
@@ -15,17 +15,23 @@ const Results = ({ searchResults, search }) => {
 
   const results = searchResults.data.included.map(({ id, attributes }) => {
     const job = attributes;
-
+    console.log("benefits", job);
     return (
-      <Panel header={`${job.company.name}: ${job.title} (${job.location.city})`} key={id}>
+      <Panel
+        header={`${job.company.name}: ${job.title} / ${job.workingTimes[0].title} (${job.location.city})`}
+        key={id}>
         <img style={{ float: "right" }} src={job.company.logo} alt={job.company.name} />
-        {job.location.city}
+        <p>{job.location.city}</p>
         <br />
-        <h3>Description</h3>
-        {job.previewText}
+        <h3>{`${job.company.name}: ${job.title}`}</h3>
+        <p>{job.previewText}</p>
         <br />
-        <h3>Benefits</h3>
-        {job.benefits}
+        {job.requirements !== "" && (
+          <>
+            <h3>Anforderungen</h3>
+            <p>{job.requirements}</p>
+          </>
+        )}
         <br />
         {job.closingText}
       </Panel>
